df645394d480f74ca1d1cca99a22d395419c5585
Vishwakarma Nitish <nanivish234@gmail.com>
2026-06-18 07:26:18 +0530

    Create CartPage.jsx

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,10 @@
+import Cart from '../components/Cart';
+
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+      <Cart />
+    </div>
+  );
+}
