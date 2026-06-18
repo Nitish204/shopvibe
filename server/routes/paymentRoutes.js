20d719a0e8c3fc39bfe7c6dac3f59f95343d48d0
Vishwakarma Nitish <nanivish234@gmail.com>
2026-06-18 07:48:11 +0530

    Add files via upload

---
+++ server/routes/paymentRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { verifyPayment } = require('../controllers/paymentController');
+const router = express.Router();
+
+router.post('/verify', verifyPayment);
+
+module.exports = router;
